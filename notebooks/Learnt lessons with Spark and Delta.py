490a9a6af41904197854584858a7073a094988b3
da115115 <da115115>
2023-06-02 13:25:28 +0000

    Creation

---
+++ notebooks/Learnt lessons with Spark and Delta.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC ## Overview
+# MAGIC That series of notebooks showcases a few lessons learnt while using Spark
+# MAGIC (and [Delta Lake](https://delta.io)) to process data. DataBricks is used here for convenience.
+# MAGIC However, any other notebook and/or compute engine may be used the same way,
+# MAGIC as the concepts presented here are based only on the open source version
+# MAGIC of both Spark and Delta Lake.
+# MAGIC
+# MAGIC ## References
+# MAGIC * [GitHub - DataBricks examples]() (Git repository hosting this notebook)
+# MAGIC * [Delta Lake homepage](https://delta.io)
+
+# COMMAND ----------
+
+
